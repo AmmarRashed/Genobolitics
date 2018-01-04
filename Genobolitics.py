129d6f2b4379ed3fa3e8757f1f5c05da50e962da
abdelrahman-t <talaataboudakika@std.sehir.edu.tr>
2018-01-04 23:52:14 +0300

    bug fixes, cleanup

--- Genobolitics.py
+++ Genobolitics.py
@@ -1,8 +1,8 @@
 import numpy as np
+from functools import reduce, lru_cache
 
 from metabolitics.analysis import MetaboliticsAnalysis
-
-from functools import reduce
+import GEOparse, pyhgnc
 
 
 class FoldChange:
@@ -22,35 +22,66 @@ class Genobolitics(MetaboliticsAnalysis):
     def __init__(self, *args, **kwargs):
         super(Genobolitics, self).__init__(*args, **kwargs)
         self.model.solver = 'cplex'
-        print('selected solver is: ', self.model.solver)
-        
+        self.model.solver.configuration.timeout = 10 * 60
+
     def set_objective(self, measured_genes):
         self.clean_objective()
         for r in self.model.reactions:
-            r.objective_coefficient = self.get_reaction_fold_change(r, measured_genes, duplicate_strategy=np.mean)
+            fold_change = 0
+            
+            try:
+                fold_change = self.get_reaction_fold_change(r, measured_genes)
+            except Exception as e:
+                print('could not calculate fold changes for {}'.format(r))
+            
+            r.objective_coefficient = fold_change
 
-    def get_reaction_fold_change(self, reaction, measured_genes, duplicate_strategy):
+    def get_reaction_fold_change(self, reaction, measured_genes):
         op = [('or', '+'), ('and', '-')]
-        genes = [
-            (gene, 'FoldChange({})'.format(self.get_gene_fold_change(gene, measured_genes, duplicate_strategy, nan=-1)))
-            for gene in self.get_reaction_genes(reaction)]
-        expr = reduce(lambda x, y: x.replace(*y), op + genes, reaction.gene_reaction_rule)
-        if expr == "":
-            return 0.0
-        return eval(expr).fold_change
+        genes = [(gene, 'FoldChange({})'.format(self.get_gene_fold_change(gene, measured_genes))) for gene in
+                 self.get_reaction_genes(reaction)]
 
-    def get_gene_fold_change(self, gene, measured_genes, duplicate_strategy=np.mean, nan=-1):
-        try:
-            return duplicate_strategy(measured_genes[gene])
-        except KeyError:
-            return nan
+        expr = reduce(lambda x, y: x.replace(*y), op + genes, reaction.gene_reaction_rule)
+
+        return eval(expr).fold_change if expr != '' else 0
+
+    def get_gene_fold_change(self, gene, measured_genes):
+        return measured_genes[gene]
 
     def get_reaction_genes(self, reaction):
-        return [g.id for g in list(reaction.genes)]
+        return [g.id for g in reaction.genes]
 
 
+@lru_cache(maxsize=None)
+def lookup_gene(gene_symbol):
+    try:
+        query = lookup_gene.query
+    except Exception as e:
+        lookup_gene.query = query = pyhgnc.query()
 
-# measured_genes = pickle.load(open("datasets/gene_data","rb"))
-# measured_genes_sample_1 = measured_genes[0]
-# gb = Genobolitics("recon2")
-# gb.set_objective(measured_genes_sample_1)
+    return query.hgnc(symbol=gene_symbol)
+
+
+def get_genes_fold_changes(sample):
+    genes_fold_changes = dict()
+    for gene in np.unique(sample.index):
+        hgnc = lookup_gene(gene)
+
+        if hgnc:
+            genes_fold_changes["HGNC:{}".format(hgnc[0].identifier)] = np.median(sample.loc[gene])
+
+    return genes_fold_changes
+
+
+def parse_database(geo_database_name, labels, index_column="IDENTIFIER"):
+    geo_df = GEOparse.get_GEO(geo=geo_database_name).table.dropna().set_index(index_column)
+    samples_columns = [column for column in geo_df.columns if column.startswith('GSM')]
+
+    X, y = [], []
+    for sample in labels.keys():
+        gene_fold_change = get_genes_fold_changes(geo_df[[sample]])
+        X.append(gene_fold_change)
+        y.append(labels[sample])
+        print("{} added with length {}".format(sample, len(gene_fold_change)))
+
+    return X, y
